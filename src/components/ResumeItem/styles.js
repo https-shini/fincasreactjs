34b264a48d0c33f2cf65d2d40eff6fe9ffdb3040
https-shini <guilhermedesouzacruz80@gmail.com>
2024-04-29 08:18:37 -0300

    new commit

---
+++ src/components/ResumeItem/styles.js
@@ -0,0 +1,53 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-color: #fff;
+    box-shadow: rgba(0, 0, 0, 0.09) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px,
+        rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px,
+        rgba(0, 0, 0, 0.09) 0px 32px 16px;
+    border-radius: 5px;
+    padding: 5px 15px;
+    width: 30%;
+
+    @media (max-width: 750px) {
+        width: 20%;
+
+        p {
+            font-size: 12px;
+        }
+
+        span {
+            font-size: 20px;
+        }
+
+        svg {
+            display: none;
+        }
+    }
+`;
+
+export const Header = styled.header`
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    width: 100%;
+    gap: 10px;
+    margin: 20px auto;
+
+    svg {
+        width: 25px;
+        height: 25px;
+    }
+`;
+
+export const HeaderTitle = styled.p`
+    font-size: 20px;
+`;
+
+export const Total = styled.span`
+    font-size: 30px;
+    font-weight: bold;
+`;
